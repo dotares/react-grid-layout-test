b0764568fee702ccb9d1a7df3031fcc1aaaa634e
fibbbonachos <arish2k3@gmail.com>
2023-10-20 13:39:16 +0530

    add: resizable component

--- my-app/src/app/_components/GridComponent.tsx
+++ my-app/src/app/_components/GridComponent.tsx
@@ -1,7 +1,18 @@
 "use client";
+import { Resizable } from "re-resizable";
 
 const GridComponent = () => {
-    return <div>This is a grid</div>;
+    return (
+        <Resizable
+            className="bg-zinc-800 text-white"
+            maxWidth={200}
+            minHeight={100}
+            defaultSize={{ width: 200, height: 100 }}
+            grid={[0, 100]}
+        >
+            sample resizable component
+        </Resizable>
+    );
 };
 
 export default GridComponent;
